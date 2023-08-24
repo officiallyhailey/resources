d1c696fdda33d46eb04ab7d172a6a2e644b0c86b
[Hailey G] <[haileydgrace@gmail.com]>
2023-08-24 03:10:58 -0400

    changes minimal

---
+++ js/minimal.js
@@ -0,0 +1,41 @@
+//panel code
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach ((panel) => {
+panel.addEventListener('click', () => {
+    removeActiveClasses()
+    panel.classList.add('active')
+})
+})
+
+
+panels.forEach ((panel) => {
+panel.addEventListener('dblclick', () => {
+    removeActiveClasses()
+    panel.classList.remove('active')
+    panel.classList.add('panel')
+})
+})
+    
+
+function removeActiveClasses() {
+    panels.forEach( panel => {
+        panel.classList.remove('active')
+    })
+}
+
+//menu code 
+
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+open.addEventListener('click', () => container.classList.add('show-nav'))
+
+close.addEventListener('click', () => container.classList.remove('show-nav'))
+
+//header text code
+
+//stats code 
+

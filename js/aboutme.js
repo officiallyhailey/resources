ebd89575d389b8be0559cee3119df5f6b697b455
[Hailey G] <[haileydgrace@gmail.com]>
2023-09-10 06:45:25 -0400

    aboutme.html changes

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,31 +1,28 @@
-window.addEventListener("DOMContentLoaded",() => {
-	const c = new CopypasteKeyboard(".keyboard");
-});
 
-class CopypasteKeyboard {
-	constructor(el) {
-		this.el = document.querySelector(el);
 
-		this.init();
-	}
-	get isMac() {
-		return /(Mac|iP[ahno]+[de])/i.test(navigator.userAgent);
-	}
-	init() {
-		window.addEventListener("keydown",this.keyAction.bind(this,true));
-		window.addEventListener("keyup",this.keyAction.bind(this,false));
-	}
-	keyAction(down,e) {
-		let { key } = e;
-		const isCmd = key === "Meta" && this.isMac;
-		const isCtrl = key === "Control" && !this.isMac; // for non-Apple only
+function showBox2() {
+// Get the button element
 
-		if (isCmd || isCtrl) key = "cmd";
 
-		const keyEl = this.el?.querySelector(`[data-key="${key}"]`);
-		const activeClass = "active";
+// Get the box2 element
+let box2 = document.getElementById('box2');
+// Add an event listener to the button for the click event
 
-		if (down === true) keyEl?.classList.add(activeClass);
-		else keyEl?.classList.remove(activeClass);
-	}
-}
+    // Show the box2 element by removing the 'hidden' class
+ 
+    if (box2.classList.contains('hidden')){
+        box2.classList.remove('hidden')}
+        else {box2.classList.add('hidden')}
+    }
+  
+
+
+
+
+      
+
+
+
+
+
+

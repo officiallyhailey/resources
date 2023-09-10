97ebc2313a35dcf9cbc656a8f20397801cec5496
[Hailey G] <[haileydgrace@gmail.com]>
2023-09-10 18:52:36 -0400

    changes

--- js/aboutme.js
+++ js/aboutme.js
@@ -28,3 +28,34 @@ function closeAllElements() {
     });
 }
 
+
+
+
+//main content
+
+let content = document.querySelectorAll(".content");
+
+content.forEach(function (button) {
+    button.addEventListener("click", function () {
+        let targetElementId = button.getAttribute("data-target");
+        let targetElement = document.getElementById(targetElementId);
+
+        if (targetElement.classList.contains("hidden")) {
+            // Open the target element and close any other elements
+            closeAllElements();
+            targetElement.classList.remove("hidden");
+        } else {
+            // Close the target element
+            targetElement.classList.add("hidden");
+        }
+    });
+});
+
+function closeAllElements() {
+    let elements = document.querySelectorAll(".content");
+    elements.forEach(function (element) {
+        let targetId = element.getAttribute("data-target");
+        let targetElement = document.getElementById(targetId);
+        targetElement.classList.add("hidden");
+    });
+}

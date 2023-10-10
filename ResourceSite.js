55ef1f907f501350094da4faf2a133ff788da96f
[Hailey G] <[haileydgrace@gmail.com]>
2023-10-10 01:43:37 -0400

    css changes

--- ResourceSite.js
+++ ResourceSite.js
@@ -1,3 +1,9 @@
-let date = new Date();
-date = date.toLocaleDateString()
+let today = new Date();
+date = today.toLocaleDateString()
 document.querySelector('.todays-date').innerHTML = date;
+
+let time = today.toLocaleTimeString();
+let timeElement = document.querySelector('.time');
+timeElement.innerHTML = time;
+
+console.log(time)
